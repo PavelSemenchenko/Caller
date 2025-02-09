ec3a92920f2b5cdc14e86f0ca73ebf29381204b6
Leksovich <imagemakerspa@gmail.com>
2025-02-09 14:30:23 +0200

    Initial Commit

---
+++ Caller/CallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallerApp.swift
+//  Caller
+//
+//  Created by Pavel Semenchenko on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caller
+//
+//  Created by Pavel Semenchenko on 09.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
